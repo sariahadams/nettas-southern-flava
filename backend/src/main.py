c1b72d1e563508168a4910fe9be6833e134c0f0c
Earl Tankard, Jr., Ph.D <theflashtank21@gmail.com>
2025-01-03 02:02:44 -0500

    refactor(chore): additional origins and minor reformatting

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from typing import Dict
+from typing import Dict, List
 
 
 app: FastAPI = FastAPI()
+# origins: List[str] = ["*"]
+origins: List[str] = [
+    "http://localhost:3000",  # localhost
+    "http://192.168.1.60:3000",  # Network
+]
 
 # Add middleware to handle Cross-Origin Resource Sharing (CORS)
 app.add_middleware(
     CORSMiddleware,
     # TODO: Replace "localhost:3000" with the actual frontend URL or load it from a configuration file.
-    allow_origins=[
-        "localhost:3000"
-    ],  # List of allowed origins (domains) that can interact with this API.
+    allow_origins=origins,  # List of allowed origins (domains) that can interact with this API.
     allow_credentials=True,  # Allow sending cookies and other credentials in cross-origin requests.
     allow_methods=[
         "*"
